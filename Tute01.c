eb0ff4c8b1d1842e91df90d6f4b1fc8a2d2a258f
it21390128 <99236910+it21390128@users.noreply.github.com>
2022-02-08 11:04:39 +0530

    Update Tute01.c

--- Tute01.c
+++ Tute01.c
@@ -5,7 +5,14 @@
 #include <stdio.h>
 
 int main() {
-  
-  return 0;
+  int mark1,mark2;
+   float avg;
+   	printf("input marks for the 1st module - ");
+	scanf ("%d",&mark1);
+	   printf("input marks for the 2nd module - ");
+	scanf ("%d",&mark2);
+   avg = (mark1+mark2)/2;
+	printf("%.2f",avg);
+   return 0;
 }
 
